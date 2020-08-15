2af705dc719b1c9099b0db2b3d8625039e8e919d
Edgars-Cirulis <edgarsjc5@gmail.com>
2020-08-15 09:27:50 +0300

    ginkgo:  Clean Overlays / Add Max visible notification icons overlay

--- device.mk
+++ device.mk
@@ -18,14 +18,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
-    $(LOCAL_PATH)/overlay-system
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay 
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
