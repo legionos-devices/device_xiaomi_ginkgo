7b9d1476262d79552e37a4178f322b75f4f0c3ed
anupam41 <anupamsamanta41@gmail.com>
2021-09-15 20:24:53 +0000

    ginkgo: Add vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+rm -rf hardware/qcom-caf/sm8150/display
+git clone https://github.com/trinket-devs/hardware_qcom-caf_sm8150_display.git -b 11.0 hardware/qcom-caf/sm8150/display
+git clone https://github.com/kdrag0n/proton-clang.git --depth=1 prebuilts/clang/host/linux-x86/clang-13.0
